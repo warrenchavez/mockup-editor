a49edecd7ec32b1827415dff20e98b9f8c22964f
Warren Chavez <warrenchavez.09@gmail.com>
2018-07-26 20:57:03 +0800

    Add New application function and view application function

--- js/main.js
+++ js/main.js
@@ -56,23 +56,60 @@ jQuery(function($) {
             $(this).closest('li').find('a.modal-edit-palette-pattern').show();
     });
 
+    // SINGLE SELECTION
     $('.con-select .btn-selection-choice').click(function() {
         $(this).closest('.con-select').find('.btn-selection-choice').removeClass('uk-active');
         $(this).addClass('uk-active');
-        if ($(this).hasClass('btn-choose-numbers-colors-none')) {
+    });
+
+    // TOGGLE-CHOOSE-SIZE
+    $('.con-select.con-toggle .btn-selection-choice').click(function() {
+        if ($(this).hasClass('toggle-hide')) {
             $(this).closest('li').find('.con-choose-numbers-colors').hide();
         }
         else
             $(this).closest('li').find('.con-choose-numbers-colors').show();
     });
 
-    $('.con-select .btn-selection-choice.btn-choose-numbers-colors-none').each(function(){
+    $('.con-select.con-toggle .btn-selection-choice.toggle-hide').each(function(){
         if ($(this).hasClass('uk-active')) {
             $(this).closest('li').find('.con-choose-numbers-colors').hide();
         }
         else
             $(this).closest('li').find('.con-choose-numbers-colors').show();
     });
+
+
+    // TOGGLE-VIEW-HIDE
+    $('.con-select.con-toggle .btn-selection-choice').click(function() {
+        if ($(this).hasClass('toggle-hide')) {
+            $(this).closest('li').find('.con-input-object').hide();
+        }
+        else
+            $(this).closest('li').find('.con-input-object').show();
+    });
+
+    $('.con-select.con-toggle .btn-selection-choice.toggle-hide').each(function(){
+        if ($(this).hasClass('uk-active')) {
+            $(this).closest('li').find('.con-input-object').hide();
+        }
+        else
+            $(this).closest('li').find('.con-input-object').show();
+    });
+
+
+    // ADD-APPLICATION
+    $('.con-select.con-toggle .btn-selection-choice').click(function() {
+        if ($(this).hasClass('toggle-show')) {
+            $(this).toggleClass('bgc-dark').toggleClass('fc-light');
+            $(this).closest('li').find('.con-add-application').toggle();
+        }
+        else if ($(this).hasClass('toggle-hide')) {
+            $(this).closest('li').find('.con-add-application').hide();
+            $(this).closest('.con-select.con-toggle').find('.btn-selection-choice').removeClass('bgc-dark').removeClass('fc-light').addClass('bdr-light');
+        }
+    });
+
 
     $(".slider").roundSlider({
         radius: 70,
